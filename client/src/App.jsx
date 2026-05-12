4ad14cd75c31342b08a837708dbf40edafb39438
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-12 22:37:15 +0530

    fix some syntax errors

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
@@ -7,48 +8,35 @@ import MovieCard from './components/MovieCard'
 
 function App() {
   const [movies, setMovies] = useState([])
-
   const [loading, setLoading] = useState(true)
 
-  const fetchMovie = async ()=>{
+  const fetchMovie = async () => {
     try {
-      const response = await axios.get(
-        "https://api.tvmaze.com/shows"
-      );
-
-      setMovies(response.data.slice(0,20));
-      setLoading(false);
-      
+      const response = await axios.get('https://api.tvmaze.com/shows')
+      setMovies(response.data.slice(0, 20))
     } catch (error) {
-      console.log(error)
+      console.error(error)
+    } finally {
       setLoading(false)
     }
   }
-  useEffect(()=>{
-    const timer = setTimeout(()=>{
-      setLoading(false)
-    },1000);
-  return ()=> clearTimeout(timer)
-    fetchMovie();
-  },[]);
 
-  if(loading){
+  useEffect(() => {
+    fetchMovie()
+  }, [])
+
+  if (loading) {
     return <h1>Loading...</h1>
   }
-  
+
   return (
     <div className="container">
-      <h1 className='title'>Movie Explorer</h1>
+      <h1 className="title">Movie Explorer</h1>
 
-      {movies.map((movie)=>{
-        <MovieCard>
-          key={movie.id}
-          movie={movie}
-        </MovieCard>
-      })}
-     
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
     </div>
-     
   )
 }
 
--- client/src/components/MovieCard.jsx
+++ client/src/components/MovieCard.jsx
@@ -3,15 +3,15 @@ import React from "react";
 function MovieCard({ movie }) {
   return (
     <div className="card">
-      <img src="movie.image?.medium" alt="movie.name" />
+      <img src={movie.image?.medium} alt={movie.name} />
 
       <div className="card-content">
         <h2>{movie.name}</h2>
       </div>
 
-      <p>Rating:{movie.rating.avarage}</p>
+      <p>Rating: {movie.rating?.average ?? "N/A"}</p>
 
-      <p>Language:{movie.language}</p>
+      <p>Language: {movie.language}</p>
     </div>
   );
 }
