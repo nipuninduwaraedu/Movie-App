3a57cc3f729f636e1aa49d8ba915fb4fcfcbc7ae
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-12 21:45:00 +0530

    create App.jsx  for  fetch data from API and display Loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [movie, setMovie] = useState([])
 
+  const [loading, setLoading] = useState(true)
+
+  const fetchMovie = async ()=>{
+    try {
+      const response = await axios.get(
+        "https://api.tvmaze.com/shows"
+      );
+
+      setMovie(response.data.slice(0,20));
+      setLoading(false);
+      
+    } catch (error) {
+      console.log(error)
+      setLoading(false)
+    }
+  }
+  useEffect(()=>{
+    fetchMovie();
+  },[]);
+
+  if(loading){
+    return <h1>Loading...</h1>
+  }
+  
   return (
-    
+    <div className="container">
+      <h1 className='title'>Movie Explorer</h1>
+     
+    </div>
      
   )
 }
