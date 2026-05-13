3d07f2ec8e551f4326a6aeba834662fa012d5cc2
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-13 08:19:51 +0530

    add search satet in App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import "./Styles/Home.css"
+import "./Styles/Home.css";
 import MovieCard from "./components/MovieCard";
-
+import Navbar from "./components/Navbar";
 
 function App() {
   const [movies, setMovies] = useState([]);
   const [loading, setLoading] = useState(true);
+  const [search, setSearch] = useState("");
 
   const fetchMovie = async () => {
     try {
@@ -23,15 +24,23 @@ function App() {
     fetchMovie();
   }, []);
 
+  const filterMovie = movies.filter((movie)=>{
+    movie.name 
+          .toLowerCase()
+          .includes(search.toLocaleLowerCase())
+  })
+
   if (loading) {
     return <h1>Loading...</h1>;
   }
 
   return (
     <div className="container">
+      <Navbar search={search} setSearch={setSearch} />
+
       <h1 className="title">Movie Explorer</h1>
       <div className="movie-grid">
-        {movies.map((movie) => (
+        {filterMovie.map((movie) => (
           <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
