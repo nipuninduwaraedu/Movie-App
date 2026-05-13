4f27b72db2fd2fac662240ced6e4909237c94e88
Nipun Induwara <nipuninduwara.edu@gmail.com>
2026-05-13 08:11:13 +0530

    make Navbar

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+   <nav className='navbar'>
+    <h1 className='logo'>Movie App</h1>
+    <input
+    type='text'
+    placeholder='search movie'
+    value={search}
+    onChange={(e)=>
+        setSearch(e.target.value)
+    }
+    className='search'/>
+    
+   </nav>
+  )
+}
+
+export default Navbar;
